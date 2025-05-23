36e9112cd4dbd4b33f00c06addaf9e2f8dfddf58
Christopher Engelhart <62675909+csengelhart@users.noreply.github.com>
2025-05-23 06:10:27 -0500

    Update Gold product object.sql
    Added header

--- Scripts/Gold/Gold product object.sql
+++ Scripts/Gold/Gold product object.sql
@@ -1,3 +1,21 @@
+/*
+	=======================================================================================================================
+	gold.dim_product View Creation
+	=======================================================================================================================
+	
+	
+	This SQL script creates a gold.dim_products view, designed as a dimension table for product information.
+	
+	The view is constructed by:
+	
+	Selecting product details from silver.crm_prd_info.
+	Generating a unique product_key using a ROW_NUMBER() window function, ordered by prd_start_dt and prd_key to ensure consistent numbering.
+	Enriching product attributes by joining with silver.erp_px_cat_g1v2 to include category, subcategory, and maintenance information.
+	Filtering out historical product data by excluding records where prd_end_dt is not NULL, retaining only current product information.
+	Renaming columns for improved readability and alignment with the gold layer's naming conventions.
+*/
+
+
 CREATE VIEW gold.dim_products AS
 SELECT
 	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
