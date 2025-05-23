c8fc10d59d5fb95e72fbaf6c39c75177368c7f8b
Christopher Engelhart <62675909+csengelhart@users.noreply.github.com>
2025-05-23 01:06:54 -0500

    Update Silver erp_px_cat_g1v2 data quality checksql.sql
    Added header

--- tests/Quality Checks/Silver/Silver erp_px_cat_g1v2 data quality checksql.sql
+++ tests/Quality Checks/Silver/Silver erp_px_cat_g1v2 data quality checksql.sql
@@ -1,17 +1,23 @@
+/*
+	This script performs quality checks on silver.erp_px_cat_g1v2 table.
+	Quality checks such as checking for unwanted spaces in the fields cat, subcat, and maintenance.
+	Also checking data consistency and standardization in each field.
+*/
+
 SELECT
 	id,
 	cat,
 	subcat,
 	maintenance
 FROM
-	bronze.erp_px_cat_g1v2;
+	silver.erp_px_cat_g1v2;
 
 -- Check for unwanted spaces in cat, subcat, maintenance field 
 -- bronze result: no unwanted spaces in any fields
 SELECT 
 	*
 FROM
-	bronze.erp_px_cat_g1v2
+	silver.erp_px_cat_g1v2
 WHERE
 	cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance);
 
@@ -21,14 +27,14 @@ WHERE
 SELECT 
 	DISTINCT cat
 FROM
-	bronze.erp_px_cat_g1v2;
+	silver.erp_px_cat_g1v2;
 
 SELECT
 	DISTINCT subcat
 FROM
-	bronze.erp_px_cat_g1v2;
+	silver.erp_px_cat_g1v2;
 
 SELECT
 	DISTINCT maintenance
 FROM
-	bronze.erp_px_cat_g1v2;
+	silver.erp_px_cat_g1v2;
